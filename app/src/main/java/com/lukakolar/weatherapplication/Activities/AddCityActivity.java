7e0908714d25cccce456cdc0a558093c4724ccf7
Luka Kolar <kolarluka96@gmail.com>
2016-09-25 19:12:25 +0200

    Created activities

---
+++ app/src/main/java/com/lukakolar/weatherapplication/Activities/AddCityActivity.java
@@ -0,0 +1,15 @@
+package com.lukakolar.weatherapplication.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lukakolar.weatherapplication.R;
+
+public class AddCityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_city);
+    }
+}
